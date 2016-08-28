8fdfa15355fe97f22807827dbcaaacda7cd8e98e
Nate Blain <blain89@gmail.com>
2016-08-28 11:36:14 -0700

    [chore] add bower_componenets to gitingnore

---
+++ client/index.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $.ajax({
+        url: '/weather',
+        dataType: 'text',
+        success: function(text) {
+           // $('#table').append(text); // refresh every 30 seconds
+           console.log(text);
+        }
+    })
+});
