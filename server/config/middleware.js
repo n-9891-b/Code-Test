74f2d3e4f03918073f64e2f815321dadf8286cd8
Nate Blain <blain89@gmail.com>
2016-08-29 20:34:42 -0700

    [feat] add route for getting soccer data

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -1,8 +1,12 @@
 var weather = require('./weather');
+var soccer = require('./soccer');
 
 module.exports = function(app, express) {
 
   app.use(express.static(__dirname + '/../../client'));
 
   app.get('/weather', weather.provideWeatherData);
+
+  app.get('/soccer', soccer.provideSoccerData);
+
 };
