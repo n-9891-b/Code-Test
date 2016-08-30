e0c50a22ebe67f1f5ecd26235afb02aadc604024
Nate Blain <blain89@gmail.com>
2016-08-29 20:33:51 -0700

    [feat] add click event for getting soccer data

--- client/index.js
+++ client/index.js
@@ -1,11 +1,23 @@
-$(document).ready(function(){
-    $.ajax({
-        url: '/weather',
-        dataType: 'text',
-        success: function(text) {
-           // $('#table').append(text); // refresh every 30 seconds
-           console.log(text);
-           //this is a change
-        }
-    })
+$(document).ready(function() {
+    $('.temp').on('click', function() {
+      $.ajax({
+          url: '/weather',
+          dataType: 'json',
+          success: function(data) {
+             console.log(data);
+             $('.temp').append('Day: ' + data.day + ' | High: ' + data.high + ' | Low: ' + data.low);
+          }
+      });
+    });
+
+    $('.soccer').on('click', function(){
+      $.ajax({
+          url: '/soccer',
+          dataType: 'json',
+          success: function(data) {
+             console.log(data);
+             $('.soccer').append('Team: ' + data.team + ' | Goal Difference: ' + data.goalDifference);
+          }
+      });
+    });
 });
