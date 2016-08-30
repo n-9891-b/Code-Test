a746c95ab928e604a9ccb6e327d112e14d32ed6b
Nate Blain <blain89@gmail.com>
2016-08-30 14:49:30 -0700

    [chore] change appended data to html

--- client/index.js
+++ client/index.js
@@ -1,23 +1,28 @@
 $(document).ready(function() {
+
     $('.temp').on('click', function() {
+      $('.temp').off('click');
       $.ajax({
           url: '/weather',
           dataType: 'json',
           success: function(data) {
-             console.log(data);
-             $('.temp').append('Day: ' + data.day + ' | High: ' + data.high + ' | Low: ' + data.low);
+             var html = '<p class="result">Day: ' + data.day + '</p><p class="result">High: ' + data.high + '</p><p class="result">Low: ' + data.low + '</p>';
+             $('.temp').append(html);
           }
       });
     });
 
     $('.soccer').on('click', function(){
+      $('.soccer').off('click');
       $.ajax({
           url: '/soccer',
           dataType: 'json',
           success: function(data) {
-             console.log(data);
-             $('.soccer').append('Team: ' + data.team + ' | Goal Difference: ' + data.goalDifference);
+             var team = data.team.replace('_', ' ');
+             var html = '<p class="result">Team: ' + team + '</p><p class="result">Goal Difference: ' + data.goalDifference + '</p>';
+             $('.soccer').append(html);
           }
       });
     });
+
 });
